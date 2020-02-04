48a4e0f53674d3c30d94f60cdb222f5250182e4e
Phillip Malboeuf <phil@phils.computer>
2020-02-04 17:12:51 -0500

    invited scroll fix

--- components/event_form.tsx
+++ components/event_form.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from 'react'
 import { Observer } from 'mobx-react'
 import { Instance } from 'mobx-state-tree'
 
-import { Text, View } from 'react-native'
+import { Text, View, ScrollView } from 'react-native'
 import { NativeRouter, Switch, Route, Link, useHistory } from 'react-router-native'
 import { Button, Appbar, List } from 'react-native-paper'
 import { pick } from 'dot-object'
@@ -171,25 +171,27 @@ export const EventForm: FunctionComponent<{
           Who would you like to invite to this event?
         </Subtitle>
 
-        <FormContext.Consumer>
-          {({ values, onChange }) =>
-            <List.Section>
-              <List.Subheader style={{ textAlign: 'right' }}>{values.invited
-                ? Object.keys(values.invited).filter(id => values.invited[id] === true).length
-                : 0} invited</List.Subheader>
-              {Array.from(store.friends.values()).map(friend => {
-                const name = `invited.${friend.id}`
-                return <List.Item key={friend.id} title={`${friend.first_name} ${friend.last_name}`}
-                  onPress={() => onChange(name, pick(name, values) !== undefined ? !pick(name, values) : true)}
-                  left={() => <Avatar {...friend} small />}
-                  right={() => <Input name={`invited.${friend.id}`} type='checkbox' />} />
-              })}
-              {/* {Array.from(store.groups.values()).map(group =>
-                <List.Item key={group.id} title={group.name}
-                  right />
-              )} */}
-            </List.Section>
-        }</FormContext.Consumer>
+        <ScrollView>
+          <FormContext.Consumer>
+            {({ values, onChange }) =>
+              <List.Section>
+                <List.Subheader style={{ textAlign: 'right' }}>{values.invited
+                  ? Object.keys(values.invited).filter(id => values.invited[id] === true).length
+                  : 0} invited</List.Subheader>
+                {Array.from(store.friends.values()).map(friend => {
+                  const name = `invited.${friend.id}`
+                  return <List.Item key={friend.id} title={`${friend.first_name} ${friend.last_name}`}
+                    onPress={() => onChange(name, pick(name, values) !== undefined ? !pick(name, values) : true)}
+                    left={() => <Avatar {...friend} small />}
+                    right={() => <Input name={`invited.${friend.id}`} type='checkbox' />} />
+                })}
+                {/* {Array.from(store.groups.values()).map(group =>
+                  <List.Item key={group.id} title={group.name}
+                    right />
+                )} */}
+              </List.Section>
+          }</FormContext.Consumer>
+        </ScrollView>
       </>,
       <Center>
         <Title>
--- components/group_form.tsx
+++ components/group_form.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from 'react'
 import { Observer } from 'mobx-react'
 import { Instance } from 'mobx-state-tree'
 
-import { Text } from 'react-native'
+import { Text, ScrollView } from 'react-native'
 import { NativeRouter, Switch, Route, Link, useHistory } from 'react-router-native'
 import { Button, Appbar, List } from 'react-native-paper'
 import { pick } from 'dot-object'
@@ -58,21 +58,23 @@ export const GroupForm: FunctionComponent<{
           Who would you like to invite to the group?
         </Subtitle>
 
-        <FormContext.Consumer>
-          {({ values, onChange }) =>
-            <List.Section>
-              <List.Subheader style={{ textAlign: 'right' }}>{values.invited
-                ? Object.keys(values.invited).filter(id => values.invited[id] === true).length
-                : 0} invited</List.Subheader>
-              {Array.from(store.friends.values()).map(friend => {
-                const name = `invited.${friend.id}`
-                return <List.Item key={friend.id} title={`${friend.first_name} ${friend.last_name}`}
-                  onPress={() => onChange(name, pick(name, values) !== undefined ? !pick(name, values) : true)}
-                  left={() => <Avatar {...friend} small />}
-                  right={() => <Input name={`invited.${friend.id}`} type='checkbox' />} />
-              })}
-            </List.Section>
-        }</FormContext.Consumer>
+        <ScrollView>
+          <FormContext.Consumer>
+            {({ values, onChange }) =>
+              <List.Section>
+                <List.Subheader style={{ textAlign: 'right' }}>{values.invited
+                  ? Object.keys(values.invited).filter(id => values.invited[id] === true).length
+                  : 0} invited</List.Subheader>
+                {Array.from(store.friends.values()).map(friend => {
+                  const name = `invited.${friend.id}`
+                  return <List.Item key={friend.id} title={`${friend.first_name} ${friend.last_name}`}
+                    onPress={() => onChange(name, pick(name, values) !== undefined ? !pick(name, values) : true)}
+                    left={() => <Avatar {...friend} small />}
+                    right={() => <Input name={`invited.${friend.id}`} type='checkbox' />} />
+                })}
+              </List.Section>
+          }</FormContext.Consumer>
+        </ScrollView>
       </>,
       <Center>
         <Title>
