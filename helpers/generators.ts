6e738cd6b11abf4c2018a279518a5acaf2c4fb0f
Phillip Malboeuf <phil@phils.computer>
2020-02-07 14:39:51 -0500

    random user ids

--- helpers/generators.ts
+++ helpers/generators.ts
@@ -2,10 +2,14 @@ import { Player } from '../models/player'
 import { money, drinks, methods, teeChoices, timesOfDay } from '../components/player_form'
 
 export const players = async () => {
-  const response = await fetch('https://randomuser.me/api/?nat=us&results=10&inc=name,location,email,picture&noinfo')
+  const response = await fetch('https://randomuser.me/api/?nat=us&results=8&inc=name,location,email,picture&noinfo')
   const { results } = await response.json()
+
+  const ids = []
   
   results.forEach((result, index) => {
+    ids.push(`random-${index}`)
+
     const player = Player.create({
       id: `random-${index}`,
     })
@@ -24,7 +28,19 @@ export const players = async () => {
       drinks: [Object.keys(drinks)[Math.floor(Math.random() * 3)]],
       tee_choices: [Object.keys(teeChoices)[Math.floor(Math.random() * 5)]],
       methods: [Object.keys(methods)[Math.floor(Math.random() * 3)]],
-      friends: ['tJnyJe7FzeekxkpVko8GjWpkTen2'],
+      friends: ['tJnyJe7FzeekxkpVko8GjWpkTen2', 'XpRa0K3r4UYO5talqFUrtBxZcKI2'],
     })
   })
+
+  // const phil = Player.create({
+  //   id: 'tJnyJe7FzeekxkpVko8GjWpkTen2',
+  // })
+
+  // phil.save({ friends: ['y2DawQul8MdZHGRnLxbHRxUncM33', 'support', ...ids] })
+
+  const jesse = Player.create({
+    id: 'fR1Z5sS5yTVuxZMKTM7HBMGVl0b2',
+  })
+
+  jesse.save({ friends: ['y2DawQul8MdZHGRnLxbHRxUncM33', 'tJnyJe7FzeekxkpVko8GjWpkTen2', 'support', ...ids] })
 }
