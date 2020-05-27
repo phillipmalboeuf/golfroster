b3b4dd3e6a9ad463aaeeb99b80b67de4ee5aaef5
Phillip Malboeuf <phil@boeuf.coffee>
2020-05-27 11:14:32 -0400

    terms links

--- web/web.tsx
+++ web/web.tsx
@@ -52,8 +52,9 @@ const App = () => {
     <footer>
       <small>&copy; GolfRoster 2020</small>
       <nav>
-        <a href='/terms'>Terms &amp; Conditions</a>
-        <a href='/privacy'>Privacy Policy</a>
+        <a href={require('./terms-v0-2.doc')} download>Terms &amp; Conditions</a>
+        <a href={require('./privacy-v0-2.doc')} download>Privacy Policy</a>
+        <a href={require('./disclaimer-v0-2.doc')} download>Disclaimer</a>
       </nav>
       <a href='jesse@golfroster.com' target='_blank'>
         <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
